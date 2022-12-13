bb637c049d264878c5282987e807f8f5a3947308
iNe1t <fantiktop212@gmail.com>
2022-12-13 21:49:07 +0300

    renamed:    Assets/Scenes/SampleScene.unity -> Assets/Scenes/Lesson1.unity 	renamed:    Assets/Scenes/SampleScene.unity.meta -> Assets/Scenes/Lesson1.unity.meta 	new file:   Assets/Scenes/LessonsMenu.unity 	new file:   Assets/Scenes/LessonsMenu.unity.meta 	new file:   Assets/Scenes/MainMenu.unity 	new file:   Assets/Scenes/MainMenu.unity.meta 	new file:   Assets/Scenes/SampleScene 1.unity 	new file:   Assets/Scenes/SampleScene 1.unity.meta 	new file:   Assets/Scenes/Settings.unity 	new file:   Assets/Scenes/Settings.unity.meta 	new file:   Assets/Scripts/MenuOnClicks.cs 	new file:   Assets/Scripts/MenuOnClicks.cs.meta 	modified:   Assets/Scripts/MetronomeV2.cs 	modified:   ProjectSettings/EditorBuildSettings.asset

---
+++ Assets/Scripts/MenuOnClicks.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOnClicks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LessonsMenuScene() {
+        SceneManager.LoadScene("LessonsMenu", LoadSceneMode.Single);
+    }
+    public void SettingsScene() {
+        SceneManager.LoadScene("Settings", LoadSceneMode.Single);
+    }
+    public void Exit() {
+        Application.Quit();
+    }
+    public void LessonLoader(int LessonNum) {
+        SceneManager.LoadScene("Lesson" + LessonNum.ToString(), LoadSceneMode.Single);
+    }
+}
--- Assets/Scripts/MetronomeV2.cs
+++ Assets/Scripts/MetronomeV2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MetronomeV2 : MonoBehaviour
 {
@@ -20,10 +21,10 @@ public class MetronomeV2 : MonoBehaviour
         // bc = GameObject.Find("CatchZone").GetComponent<BeatCatcher>();
         beatTempo = bpm / 60f;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
+    IEnumerator waiter(int secs) {
+        yield return new WaitForSecondsRealtime(secs);
+    }
+    void Update(){
         allCatchedBeats = GameObject.Find("CatchZone1").GetComponent<CatchZone1>().SendNumbers() +  
                             GameObject.Find("CatchZone2").GetComponent<CatchZone2>().SendNumbers()+
                             GameObject.Find("CatchZone3").GetComponent<CatchZone3>().SendNumbers()+
@@ -31,15 +32,16 @@ public class MetronomeV2 : MonoBehaviour
         if(GameObject.Find("NoteList").transform.childCount == 0) {
            words.text = allCatchedBeats.ToString() + "/" + allBeats.ToString(); 
            Destroy(GameObject.Find("NoteList"));
-        //    bc.SendNumbers()  + "/" +
-        }
-       if(!hasStarted) {
-        if(Input.anyKeyDown) {
-            hasStarted = true;
-            words.text = "";
-        }
-       } else {
+           StartCoroutine(waiter(5));
+           SceneManager.LoadScene("LessonsMenu", LoadSceneMode.Single);
+           }
+        if(!hasStarted) {
+            if(Input.anyKeyDown) {
+                hasStarted = true;
+                words.text = "";
+            }
+        } else {
             transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
-       }
+        }
     }
 }
