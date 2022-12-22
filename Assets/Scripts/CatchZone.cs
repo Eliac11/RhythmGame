a43bbd4d56098a5553993f17b1c549e35ec85fd3
Viacheslav Andreev <maxivimax@icloud.com>
2022-12-22 12:12:57 +0300

    XD

--- Assets/Scripts/CatchZone.cs
+++ Assets/Scripts/CatchZone.cs
@@ -11,14 +11,15 @@ public class CatchZone : MonoBehaviour
     }
     float beatCount = 0f;
     public KeyCode keyToPress;
-    private void OnTriggerStay2D(Collider2D other) {
+
+    void OnTriggerStay2D(Collider2D other) {
         if(Input.GetKeyDown(keyToPress)) {
             audioSource.PlayOneShot(clip, 0.5f);
             if(other.gameObject.tag == "BeatMark") {
                 beatCount++;
                 Destroy(other.gameObject);
                 Debug.Log("Well Done!!!" + SendNumbers() + "/" + GameObject.Find("NoteList").GetComponent<MetronomeV2>().allBeats);
-        }
+            }
         }
     }
 }
