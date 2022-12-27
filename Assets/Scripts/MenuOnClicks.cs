099f9f1c8aa2a0f2b214585509b9ab948a92c4da
iNe1t <fantiktop212@gmail.com>
2022-12-27 19:05:13 +0300

    Results Window Made

--- Assets/Scripts/MenuOnClicks.cs
+++ Assets/Scripts/MenuOnClicks.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuOnClicks : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     public void LessonsMenuScene() {
         SceneManager.LoadScene("LessonsMenu", LoadSceneMode.Single);
