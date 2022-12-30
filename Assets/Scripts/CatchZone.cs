ef38c05b4116a9b60498f225585e4cbd151ebf4b
iNe1t <fantiktop212@gmail.com>
2022-12-30 12:36:10 +0300

    Zaebalsya

--- Assets/Scripts/CatchZone.cs
+++ Assets/Scripts/CatchZone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatchZone : MonoBehaviour
 {
@@ -9,11 +10,8 @@ public class CatchZone : MonoBehaviour
     public float SendNumbers() {
         return beatCount;
     }
-    float beatCount = 0f;
-    public KeyCode keyToPress;
-
-    void OnTriggerStay2D(Collider2D other) {
-        if(Input.GetKeyDown(keyToPress)) {
+    public void BeatMaker(bool canBeat, Collider2D other) {
+        if(canBeat) {
             audioSource.PlayOneShot(clip, 0.5f);
             if(other.gameObject.tag == "BeatMark") {
                 beatCount++;
@@ -22,5 +20,10 @@ public class CatchZone : MonoBehaviour
                 // Debug.Log("Well Done!!!" + SendNumbers() + "/" + GameObject.Find("NoteList").GetComponent<MetronomeV2>().allBeats);
             }
         }
+    }
+    float beatCount = 0f;
+    public KeyCode keyToPress;
+    void OnTriggerStay2D(Collider2D other) {
+        BeatMaker(true, other);
     }
 }
