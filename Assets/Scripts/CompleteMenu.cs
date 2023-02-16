1518ecbb9f6dae9138f07fdf614f37d8eb2a4f31
Ne1Tral_In_Da_Club <fantiktop212@gmail.com>
2023-02-16 18:00:14 +0300

    Сделал Мемы
    Я умничка

--- Assets/Scripts/CompleteMenu.cs
+++ Assets/Scripts/CompleteMenu.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class CompleteMenu : MonoBehaviour {
 
@@ -14,9 +15,12 @@ public class CompleteMenu : MonoBehaviour {
     public int allBeats;
     public float allCatchedBeats = 0f;
     public Sprite activestar;
+    public Image memeWindow;
     public int stars;
     public int starsNew;
     public string LessonNum;
+    public string PathToMemes; 
+    public bool memeSet = false;
 
     private string[] midis = { "TestRoad", "AllNote120", "AllNote60" };
     
@@ -26,7 +30,7 @@ public class CompleteMenu : MonoBehaviour {
 
     void Start() {
         stars = PlayerPrefs.GetInt("Lesson" + MidiToTiles.MidiName + "Stars");
-        Debug.Log(stars);
+        //Debug.Log(stars);
         LessonNum = SceneManager.GetActiveScene().name.Remove(0, 6);
     }
 
@@ -37,21 +41,28 @@ public class CompleteMenu : MonoBehaviour {
                             GameObject.Find("CatchZone3").GetComponent<CatchZone>().SendNumbers()+
                             GameObject.Find("CatchZone4").GetComponent<CatchZone>().SendNumbers();
         if(GameObject.Find("NoteList").transform.childCount == 0) {
-           tiles.text = "Tiles Tapped: " + allCatchedBeats.ToString() + "/" + allBeats.ToString();
-           percentage.text = "Complete Percent: " + Mathf.RoundToInt((allCatchedBeats/allBeats)*100);
-           if( Mathf.RoundToInt((allCatchedBeats/allBeats)*100) > 66) {
+            tiles.text = "Tiles Tapped: " + allCatchedBeats.ToString() + "/" + allBeats.ToString();
+            percentage.text = "Complete Percent: " + Mathf.RoundToInt((allCatchedBeats/allBeats)*100);
+            // Тута пиши
+            if( Mathf.RoundToInt((allCatchedBeats/allBeats)*100) > 66) {
                 for (int i = 1; i < 4; i++) {
                     GameObject.Find("Star" + i.ToString()).GetComponent<SpriteRenderer>().sprite = activestar;
                     starsNew = 3;
                 }
-           } else if(Mathf.RoundToInt((allCatchedBeats/allBeats)*100) > 33 && Mathf.RoundToInt((allCatchedBeats/allBeats)*100) < 66) {
+                StartCoroutine(LoadImageFromURL(Application.streamingAssetsPath  + "/Memes/goodmeme" + UnityEngine.Random.Range(1,6).ToString() + ".png"));
+                memeSet = true;
+            } else if(Mathf.RoundToInt((allCatchedBeats/allBeats)*100) > 33 && Mathf.RoundToInt((allCatchedBeats/allBeats)*100) < 66) {
                 for (int i = 1; i < 3; i++) {
                     GameObject.Find("Star" + i.ToString()).GetComponent<SpriteRenderer>().sprite = activestar;
                     starsNew = 2;
                 }
-           } else if(Mathf.RoundToInt((allCatchedBeats/allBeats)*100) < 33) {
-                    GameObject.Find("Star1").GetComponent<SpriteRenderer>().sprite = activestar;
-                    starsNew = 1;
+                StartCoroutine(LoadImageFromURL(Application.streamingAssetsPath  + "/Memes/normalmeme" + UnityEngine.Random.Range(1,6).ToString() + ".png"));
+                memeSet = true;
+            } else if(Mathf.RoundToInt((allCatchedBeats/allBeats)*100) < 33) {
+                GameObject.Find("Star1").GetComponent<SpriteRenderer>().sprite = activestar;
+                starsNew = 1;
+                StartCoroutine(LoadImageFromURL(Application.streamingAssetsPath  + "/Memes/badmeme" + UnityEngine.Random.Range(1,6).ToString() + ".png"));
+                memeSet = true;
             }
         }
         if (stars < starsNew) {
@@ -60,5 +71,18 @@ public class CompleteMenu : MonoBehaviour {
             PlayerPrefs.Save();
             // Debug.Log(MidiName);
         }
+        // Debug.Log(PathToMemes);
+    }
+    public IEnumerator LoadImageFromURL(string url) {
+        if (!memeSet) {
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
+
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2());
+            memeWindow.sprite = sprite;  
+            Debug.Log("Ive loaded a picture");
+        }
     }
 }
+
